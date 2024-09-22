79fdb2b0cf838b86d2673788c12dd5beba39ec06
VicDrumFar <153121611+VicDrumFar@users.noreply.github.com>
2024-09-22 18:33:01 -0300

    Update 2_visao_entregadores.py

--- pages/2_visao_entregadores.py
+++ pages/2_visao_entregadores.py
@@ -80,7 +80,7 @@ def clean_code( df1 ):
 # Import dataset
 #====================================
 
-df = pd.read_csv('train.csv')
+df = pd.read_csv('dataset/train.csv')
 
 # ==================================
 # cleaning dataset
